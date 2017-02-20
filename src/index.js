3f921a88022b2c1f1c6004c46821de026e1615f9
Ann Prokhorova <prokhorova.ann26@gmail.com>
2017-02-20 20:08:46 +0400

    project build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppContainer from './App';
 import store from './store/store';
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
